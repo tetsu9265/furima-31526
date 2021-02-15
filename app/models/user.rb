3934f7ff4501ebf280ebccbc37ee46391145d1d9
tetsu9265 <1013toru@gmail.com>
2021-02-15 11:55:45 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   PASSWORD_REGIX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :encrypted_password, with: PASSWORD_REGIX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z, message: '全角（カタカナ）を使用してください'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角（カタカナ）を使用してください' } do
     validates :family_name_kana
     validates :last_name_kana
   end
