4807de9a76861a615a33ff0064bd2dc2c0247cd3
tetsu9265 <1013toru@gmail.com>
2021-02-15 13:28:24 +0900

    ユーザー情報をDBに保存できるように設定

--- db/migrate/20210213170826_devise_create_users.rb
+++ db/migrate/20210213170826_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: ""
-      t.string :password,           null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false, default: ""
       t.string :given_name,         null: false, default: ""
       t.string :family_name_kana,   null: false, default: ""
