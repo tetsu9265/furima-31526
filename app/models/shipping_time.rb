ba716e92979cfe5318c0f116235a20b0c00d801e
tetsu9265 <1013toru@gmail.com>
2021-02-24 19:02:26 +0900

    エラー修正

--- app/models/shipping_time.rb
+++ app/models/shipping_time.rb
@@ -1,5 +1,5 @@
-class ShippngTime < ActiveHash::Base
-  data.self = [
+class ShippingTime < ActiveHash::Base
+  self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '1~2日で発送' },
     { id: 3, name: '2~3日で発送' },
@@ -8,5 +8,6 @@ class ShippngTime < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :items
-  
+
+
 end
