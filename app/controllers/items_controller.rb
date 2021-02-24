739ad7df0fd186a89c9df9e6246f5a6f85c30ae4
tetsu9265 <1013toru@gmail.com>
2021-02-24 19:38:15 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit( :name, :description, :category_id, 
+                                  :condition_id, :delivery_fee_id, :prefecture_id,
+                                  :shipping_time_id, :price)
+  end
 end
