70218ddff3e4e44078167f8a8f66f54ab5a1169f
tetsu9265 <1013toru@gmail.com>
2021-02-27 03:05:41 +0900

    itemのモデルの単体テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description      { Faker::Lorem.sentence }
     category_id      { Faker::Number.between(from: 2, to: 11) }
     condition_id     { Faker::Number.between(from: 2, to: 7) }
-    delivery_fee_id   { Faker::Number.between(from: 2, to: 3) }
+    delivery_fee_id  { Faker::Number.between(from: 2, to: 3) }
     prefecture_id    { Faker::Number.between(from: 2, to: 48) }
     shipping_time_id { Faker::Number.between(from: 2, to: 4) }
     price            { Faker::Number.between(from: 300, to: 9999999) }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できるとき' do
+      it '全ての必須商品情報があれば出品できる' do
+        expect(@item).to be_valid
+      end
+      it '価格が300円以上なら出品できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+      it '価格が9,999,999円以下なら出品できる' do
+        @item.price = 9999999
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できないとき' do
+      it '画像が存在しないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'カテゴリーが選択されていないと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it '商品の状態が選択されていないと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it '配送料の負担が選択されていないと出品できない' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+      it '発送元の地域が選択されていないと出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '発送までの日数が選択されていないと出品できない' do
+        @item.shipping_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping time must be other than 1")
+      end
+      it '価格が存在していないと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300円未満だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+      end
+      it '価格が9,999,999円を超えると出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+    end
+  end
 end
