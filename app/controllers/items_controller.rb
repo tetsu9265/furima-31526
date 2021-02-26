b2884caf52e552b17d902d643b6e810d9b951dfc
tetsu9265 <1013toru@gmail.com>
2021-02-26 13:43:33 +0900

    非ログイン状態のuserをログインページへ移動させるための記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login, except: [:index]
+
   def index
   end
 
@@ -21,5 +23,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit( :name, :description, :category_id, 
                                   :condition_id, :delivery_fee_id, :prefecture_id,
                                   :shipping_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
