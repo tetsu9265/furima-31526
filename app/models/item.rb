685ca14407d80247e544f41ab0ab5d6f81d8fe90
tetsu9265 <1013toru@gmail.com>
2021-02-26 13:44:02 +0900

    バリデージョンの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,18 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :shipping_time
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+  end
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_time_id
+  end
+  validates :price, presence: true, numericality: { only_integer: true,  greater_than: 299, less_than: 10000000 }
 end
