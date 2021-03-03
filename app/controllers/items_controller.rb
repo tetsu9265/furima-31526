d24931b841731ff46c740b1602c1648007cdb55f
tetsu9265 <1013toru@gmail.com>
2021-03-03 20:34:28 +0900

    before_actionの順序変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id != Item.find(params[:id]).user_id
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def set_item
