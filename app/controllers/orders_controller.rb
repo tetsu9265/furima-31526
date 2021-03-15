b6f8ddbb8b2df6b3ef1fd70fb2facf6697db8875
tetsu9265 <1013toru@gmail.com>
2021-03-15 13:20:30 +0900

    indexアクションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   def index
-    @order_address = OrderAddress.new.merge(user_id: current_user.id, item_id: params[:item_id])
+    @order_address = OrderAddress.new
+    @order_address.user = User.find(current_user.id)
+    @order_address.item = Item.find(params[:item_id])
   end
 end
