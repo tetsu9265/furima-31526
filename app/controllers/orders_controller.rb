2a0bed6af4fa46eb0f5a13e1921879c7905d15e7
tetsu9265 <1013toru@gmail.com>
2021-03-15 13:07:48 +0900

    orderaddressのインスタンスにuser,itemの情報を渡す。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
   def index
-    @order_address = OrderAddress.new
+    @order_address = OrderAddress.new.merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
