fef3426a23a0a8471d03caedebd2fa31dcf79a77
tetsu9265 <1013toru@gmail.com>
2021-03-15 14:15:58 +0900

    ordersのcreateアクションの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,22 @@ class OrdersController < ApplicationController
     @order_address.user = User.find(current_user.id)
     @order_address.item = Item.find(params[:item_id])
   end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    @order_address.user = User.find(current_user.id)
+    @order_address.item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to item_path(@order_address.item.id)
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipalities, :block_number, :building, :phone_number)
+  end
 end
