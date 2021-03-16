b529b527166359c2c3631be7f74fd5e00276b1aa
tetsu9265 <1013toru@gmail.com>
2021-03-16 09:54:45 +0900

    factorybotの作成

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { '2' }
+    municipalities { '足立区' }
+    block_number { '1-1' }
+    building { '東京ハイツ' }
+    phone_number { '09012345678' }
+
+    association :user
+    association :item
+  end
+end
