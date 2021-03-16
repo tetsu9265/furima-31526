ce1e17aff74bacc6bb6cccf3ae36103005bb9446
tetsu9265 <1013toru@gmail.com>
2021-03-16 16:42:43 +0900

    prefecture_idの型の修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567' }
-    prefecture_id { '2' }
+    prefecture_id { 2 }
     municipalities { '足立区' }
     block_number { '1-1' }
     building { '東京ハイツ' }
