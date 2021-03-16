290c98d53c7323bda47ac2a38f5acf0ddc99b5ec
tetsu9265 <1013toru@gmail.com>
2021-03-16 10:38:56 +0900

    orderaddressモデルの単体テストコードの記述

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '注文情報の保存' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+
+    it '全ての値が正しく入力されていれば保存できること' do
+      expect(@order_address).to be_valid
+    end
+    it 'postal_codeが空だと保存できないこと' do
+      @order_address.postal_code = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeが半角のハイフンを含んだ正しい形でないと保存できないこと' do
+      @order_address.postal_code = 1234567
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'prefectureを選択していないと保存できないこと' do
+      @order_address.prefecture_id = 1
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it 'municipalitiesが空だと保存できないこと' do
+      @order_address.municipalities = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Municipalities can't be blank")
+    end
+    it 'block_numberが空だと保存できないこと' do
+      @order_address.block_number = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Block number can't be blank")
+    end
+    it 'buildingは空でも保存できること' do
+      @order_address.building = ''
+      expect(@order_address).to be_valid
+    end
+    it 'phone_numberが空だと保存できないこと' do
+      @order_address.phone_number = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberが11桁以内でないと保存できないこと' do
+      @order_address.phone_number = 123456123456
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+    end
+  end
+end
