dc4561c1d802ec52674fad794a889a8d12d10f5e
tetsu9265 <1013toru@gmail.com>
2021-03-16 09:53:20 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,11 +5,11 @@ class OrderAddress
   with_options presence: true do
     validates :user
     validates :item
-    validates :postal_code
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :block_number
-    validates :phone_number
+    validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11 }
   end
 
   def save
