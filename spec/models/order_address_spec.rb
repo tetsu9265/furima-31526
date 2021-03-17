5ee3bc36e1a2427f98cf72d3c3542ddc1c3df8eb
tetsu9265 <1013toru@gmail.com>
2021-03-17 18:50:55 +0900

    テストコードを正常系と異常系に整理

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -6,52 +6,58 @@ RSpec.describe OrderAddress, type: :model do
       @order_address = FactoryBot.build(:order_address)
     end
 
-    it '全ての値が正しく入力されていれば保存できること' do
-      expect(@order_address).to be_valid
+    context '注文情報が保存できる時' do
+      it '全ての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
     end
-    it 'postal_codeが空だと保存できないこと' do
-      @order_address.postal_code = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
-    end
-    it 'postal_codeが半角のハイフンを含んだ正しい形でないと保存できないこと' do
-      @order_address.postal_code = 1_234_567
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
-    end
-    it 'prefectureを選択していないと保存できないこと' do
-      @order_address.prefecture_id = 1
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
-    end
-    it 'municipalitiesが空だと保存できないこと' do
-      @order_address.municipalities = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Municipalities can't be blank")
-    end
-    it 'block_numberが空だと保存できないこと' do
-      @order_address.block_number = ''
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Block number can't be blank")
-    end
-    it 'buildingは空でも保存できること' do
-      @order_address.building = ''
-      expect(@order_address).to be_valid
-    end
-    it 'phone_numberが空だと保存できないこと' do
-      @order_address.phone_number = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
-    end
-    it 'phone_numberが11桁以内でないと保存できないこと' do
-      @order_address.phone_number = 123_456_123_456
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include('Phone number is too long (maximum is 11 characters)')
-    end
-    it 'tokenが空では保存できないこと' do
-      @order_address.token = nil
-      @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Token can't be blank")
+
+    context '注文情報が保存できないとき' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形でないと保存できないこと' do
+        @order_address.postal_code = 1_234_567
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'prefectureを選択していないと保存できないこと' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'municipalitiesが空だと保存できないこと' do
+        @order_address.municipalities = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Municipalities can't be blank")
+      end
+      it 'block_numberが空だと保存できないこと' do
+        @order_address.block_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Block number can't be blank")
+      end
+  
+      it 'phone_numberが空だと保存できないこと' do
+        @order_address.phone_number = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが11桁以内でないと保存できないこと' do
+        @order_address.phone_number = 123_456_123_456
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is too long (maximum is 11 characters)')
+      end
+      it 'tokenが空では保存できないこと' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
